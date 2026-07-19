ff80e47b1c6636d908c275d8c874acf85946739d
supperedit <suppereditclub@gmail.com>
2026-07-20 00:38:19 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,8 @@
 import { defineConfig } from "vite"
-import path from 'path';
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import svgr from "vite-plugin-svgr";
-
-
-function figmaAssetResolver() {
-  return {
-    name: 'figma-asset-resolver',
-    resolveId(id) {
-      if (id.startsWith('figma:asset/')) {
-        const filename = id.replace('figma:asset/', '')
-        return path.resolve(__dirname, 'src/assets', filename)
-      }
-    },
-  }
-}
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
+import svgr from "vite-plugin-svgr"
 
 export default defineConfig({
-  plugins: [
-    figmaAssetResolver(),svgr(), react(), tailwindcss()],
-});
+  plugins: [react(), tailwindcss(), svgr()],
+})
