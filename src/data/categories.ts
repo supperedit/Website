2d5417ea2e7391b5faaee056698da27a87123123
supperedit <suppereditclub@gmail.com>
2026-08-13 13:00:50 +0200

    Update categories.ts

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -5,12 +5,54 @@ export interface Category {
 }
 
 export const categories: Category[] = [
-  { slug: "cookie", name: "Bake Club", sub: "Cookies & süße Kleinigkeiten" },
-  { slug: "swirl", name: "Swirl Society", sub: "Zimtschnecken & süßes Hefegebäck" },
-  { slug: "saucy", name: "Saucy Stuff", sub: "Soßen, Dips & Aufstriche" },
-  { slug: "pickle", name: "Pickle & Ferment", sub: "Fermentiertes & Eingelegtes" },
-  { slug: "fizz", name: "Fizz & Friends", sub: "Getränke & alkoholfreie Drinks" },
-  { slug: "bites", name: "Small Bites", sub: "Snacks & kleine Gerichte" },
-  { slug: "bread", name: "Bread & Butter", sub: "Brot & Aufstriche" },
-  { slug: "pasta", name: "Pasta Night", sub: "Pasta & italienische Klassiker" },
+  { 
+    slug: "cookie", 
+    name: "Bake Club", 
+    sub: "Cookies, Kuchen & Gebäck" 
+  },
+  { 
+    slug: "swirl", 
+    name: "Swirl Society", 
+    sub: "Zimtschnecken & Hefegebäck" 
+  },
+  { 
+    slug: "saucy", 
+    name: "Saucy Stuff", 
+    sub: "Saucen, Dips & Dressings" 
+  },
+  { 
+    slug: "pickle", 
+    name: "Pickle & Ferment", 
+    sub: "Pickles & Fermentiertes" 
+  },
+  { 
+    slug: "fizz", 
+    name: "Fizz & Friends", 
+    sub: "Limonaden, Sodas & Mocktails" 
+  },
+  { 
+    slug: "bites", 
+    name: "Small Bites", 
+    sub: "Snacks, Crostini & Vorspeisen" 
+  },
+  { 
+    slug: "bread", 
+    name: "Bread & Butter", 
+    sub: "Brot, Butter & Aufstriche" 
+  },
+  { 
+    slug: "pasta", 
+    name: "Pasta Night", 
+    sub: "Pasta & schnelle Abendessen" 
+  },
+  { 
+    slug: "slow-sips", 
+    name: "Slow Sips", 
+    sub: "Matcha, Kaffee & Café-Drinks" 
+  },
+  { 
+    slug: "pantry", 
+    name: "Pantry Edit", 
+    sub: "Sirups, Öle & Vorräte" 
+  },
 ];
