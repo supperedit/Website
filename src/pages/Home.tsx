7fda86c4c3cf130dfe6f75453a96d3a4a45000f1
supperedit <suppereditclub@gmail.com>
2026-08-15 01:33:06 +0200

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -18,6 +18,8 @@ import SauceIcon from "../assets/icons/sauce.svg?react";
 import ButterIcon from "../assets/icons/butter.svg?react";
 import PastaIcon from "../assets/icons/pasta.svg?react";
 import SnackIcon from "../assets/icons/snack.svg?react";
+import SlowSipsIcon from "../assets/icons/slow-sips.svg?react";
+import PantryIcon from "../assets/icons/pantry.svg?react";
 import picnicImage from "../assets/images/picnic.jpg";
 
 const marqueeText = "Recipes for people who don't follow recipes.";
@@ -33,6 +35,8 @@ const categoryIcons: Record<string, { Icon: React.ComponentType<React.SVGProps<S
   bites: { Icon: SnackIcon, height: 110 },
   bread: { Icon: ButterIcon, height: 67 },
   pasta: { Icon: PastaIcon, height: 69 },
+  slow-sips: { Icon: SlowSipsIcon, height: 90 },
+  pantry: { Icon: PantryIcon, height: 90 },
 };
 
 const VIBES = [
