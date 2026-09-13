4eab522a51728eea5a45be48b3ebe998426b01e9
supperedit <suppereditclub@gmail.com>
2026-09-14 00:43:49 +0200

    Update image rendering in pin API response

--- functions/api/pin/[slug].ts
+++ functions/api/pin/[slug].ts
@@ -53,7 +53,7 @@ export const onRequest: PagesFunction<Env> = async (context) => {
   const cream = "#F7F6EC";
 
   const photo = entry.image
-    ? `<img src="${origin}/img/journal/${entry.slug}" style="width:100%;height:100%;object-fit:contain;" />`
+    ? `<img src="${origin}/img/journal/${entry.slug}" width="920" height="880" style="width:920px;height:880px;object-fit:contain;" />`
     : `<div style="display:flex;width:100%;height:100%;background:${cream};"></div>`;
 
   const html = `
